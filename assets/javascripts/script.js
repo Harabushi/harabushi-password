f4153dd337f33fc7e43cfa44ad71adc314363528
Colby Harris <harris.colby@gmail.com>
2022-01-16 06:10:14 -0600

    function setup and generatePassword initial

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,35 +1,53 @@
 // Assignment code here
 
 // Confirm password length
+function setLength() {
+  var passLength = prompt("How long would you like your password to be? (8-128 characters)")
+  passLength = parseInt(passLength);
+ 
+  return passLength;
+}
 
 // Confirm lowercase
 function setLowerCase() {
-  var lowerCase = prompt("do you want lowercase characters in your password? Y or N?")
+  var lowerCase = confirm("Do you want lowercase characters in your password? Ok for Yes, Cancel for No.")
+
+  return lowerCase;
 }
 
 // Confirm uppercase
 function setUpperCase() {
-  var upperCase = prompt("do you want uppercase characters in your password? Y or N?")
+  var upperCase = confirm("Do you want uppercase characters in your password? Ok for Yes, Cancel for No.")
+
+  return upperCase;
 }
 
 // Confirm numeric
 function setNumeric() {
-  var numeric = prompt("do you want numeric characters in your password? Y or N?")
+  var numeric = confirm("Do you want numeric characters in your password? Ok for Yes, Cancel for No.")
+
+  return numeric;
 }
 
 // Confirm special
 function setSpecial() {
-  var special = prompt("do you want special characters in your password? Y or N?")
+  var special = confirm("Do you want special characters in your password? Ok for Yes, Cancel for No.")
+  
+  return special;
 }
 
 // Confirm at least one type selected
 
 // Generate password
 function generatePassword() {
-  setLowerCase();
-  setUpperCase();
-  setNumeric();
-  setSpecial();
+  var passLength = setLength();
+  var lowerCase = setLowerCase();
+  var upperCase = setUpperCase();
+  var numeric = setNumeric();
+  var special = setSpecial();
+
+  console.log(passLength, lowerCase, upperCase, numeric, special)
+
 }
 
 // Get references to the #generate element
